56e2154768fc7cb5f552cbae7bdbf4c1b8f88510
Casey Metz <caseymetz@gmail.com>
2016-08-24 16:02:45 -0600

    add file structure and defaults functions

--- tests/unit-tests/index-test.js
+++ tests/unit-tests/index-test.js
@@ -1,5 +1,6 @@
 const assert = require('chai').assert;
-const changeDefault = require('../../lib/settings.js');
+const Settings = require('../../lib/scripts/settings.js');
+const Timer = require('../../lib/scripts/timer.js');
 
 describe('our test bundle', function () {
   it('should work', function () {
@@ -7,13 +8,28 @@ describe('our test bundle', function () {
   });
 });
 
+describe('settings functions', function(){
+  context('changeDefault functions', function () {
+    it('should override the default work interval', function () {
+      Settings.changeDefaults(30, 5);
+      assert.equal(Settings.workTime, 30);
+    });
+    it('should override the default break interval', function () {
+      Settings.changeDefaults(30, 10);
+      assert.equal(Settings.breakTime, 10);
+    });
+  });
 
-
-describe('changeDefault()', function () {
-  it('should reset the value of a variable', function () {
-    var num = 20;
-    console.log(changeDefault);
-    var var2 = changeDefault(num);
-    assert.equal(var2, 21);
+    context('reset defaults function', function () {
+      it('reset work interval to 25', function () {
+        Settings.workTime = 30;
+        Settings.resetDefaults();
+        assert.equal(Settings.workTime, 25);
+      });
+      it('reset break interval to 5', function () {
+        Settings.breakTime = 650;
+        Settings.resetDefaults();
+        assert.equal(Settings.breakTime, 5);
+      });
   });
 });
