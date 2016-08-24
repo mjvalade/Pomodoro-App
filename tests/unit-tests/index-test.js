510e40b2188adff9892f1a9ea5f379014cd7c05e
Casey Metz <caseymetz@gmail.com>
2016-08-24 16:30:42 -0600

    add pseudo-coded event listeners

--- tests/unit-tests/index-test.js
+++ tests/unit-tests/index-test.js
@@ -12,24 +12,24 @@ describe('settings functions', function(){
   context('changeDefault functions', function () {
     it('should override the default work interval', function () {
       Settings.changeDefaults(30, 5);
-      assert.equal(Settings.workTime, 30);
+      assert.equal(Settings.workDefault, 30);
     });
     it('should override the default break interval', function () {
       Settings.changeDefaults(30, 10);
-      assert.equal(Settings.breakTime, 10);
+      assert.equal(Settings.breakDefault, 10);
     });
   });
 
     context('reset defaults function', function () {
       it('reset work interval to 25', function () {
-        Settings.workTime = 30;
+        Settings.workDefault = 30;
         Settings.resetDefaults();
-        assert.equal(Settings.workTime, 25);
+        assert.equal(Settings.workDefault, 25);
       });
       it('reset break interval to 5', function () {
-        Settings.breakTime = 650;
+        Settings.breakDefault = 650;
         Settings.resetDefaults();
-        assert.equal(Settings.breakTime, 5);
+        assert.equal(Settings.breakDefault, 5);
       });
   });
 });
