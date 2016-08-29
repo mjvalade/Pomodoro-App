f2f9029d70084ce89d87c5dae324219eb3c87061
mjvalade <mjvalade@gmail.com>
2016-08-29 10:11:18 -0600

    mostly styling

--- tests/unit-tests/index-test.js
+++ tests/unit-tests/index-test.js
@@ -3,13 +3,13 @@ const Settings = require('../../lib/scripts/settings.js');
 const Timer = require('../../lib/scripts/timer.js');
 
 describe('our test bundle', function () {
-  it('should work', function () {
+  it('should be connected', function () {
     assert(true);
   });
 });
 
-describe('settings functions', function(){
-  context('changeDefault functions', function () {
+describe('Settings functions', function(){
+  context('changeDefaults() functions', function () {
     it('should override the default work interval', function () {
       Settings.changeDefaults(30, 5);
       assert.equal(Settings.workDefault, 30);
@@ -20,21 +20,35 @@ describe('settings functions', function(){
     });
   });
 
-    context('reset defaults function', function () {
-      it('reset work interval to 25', function () {
-        Settings.workDefault = 30;
-        Settings.resetDefaults();
-        assert.equal(Settings.workDefault, 25);
-      });
-      it('reset break interval to 5', function () {
-        Settings.breakDefault = 650;
-        Settings.resetDefaults();
-        assert.equal(Settings.breakDefault, 5);
-      });
+  context('resetDefaults() function', function () {
+    it('reset work interval to 25', function () {
+      Settings.workDefault = 30;
+      Settings.resetDefaults();
+      assert.equal(Settings.workDefault, 25);
+    });
+    it('reset break interval to 5', function () {
+      Settings.breakDefault = 650;
+      Settings.resetDefaults();
+      assert.equal(Settings.breakDefault, 5);
+    });
   });
 });
 
 describe('Timer functionality', function(){
+  context('with constructor attributes', function () {
+    it('should have a default startTime of null', function () {
+      Timer.startTime = null;
+      assert.equal(Timer.startTime, null);
+    });
+    it('should have a set() method to set startTime to Date.now()', function () {
+      Timer.startTime = time;
+      set(time = Date.now());
+      
 
+    });
+    it.skip('should have a run() method to set startTime to Date.now()', function () {
 
+    });
+
+  });
 });
